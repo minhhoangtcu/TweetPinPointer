72f569828f701c03cdbe0ce8d24102d5cfed59d4
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-10-02 00:50:44 -0500

    Changed info window to icon

--- TweetPinPointer/MapsDelegate.swift
+++ TweetPinPointer/MapsDelegate.swift
@@ -10,16 +10,17 @@ import Foundation
 import GoogleMaps
 
 class MapsDelegate: UIViewController, GMSMapViewDelegate {
+
+//    NOT NEED ANYMORE BECAUSE WE WILL NEVER HAVE INFO WINDOW
+//    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+//        let customMarker: TweetInfoWindow = TweetInfoWindow.loadFromNib()
+//        customMarker.text.text = marker.tweet.text
+//        // change the image
+//        // change the color of the text for neg/pos
+//        return customMarker
+//    }
     
-    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
-        let customMarker: TweetInfoWindow = TweetInfoWindow.loadFromNib()
-        customMarker.text.text = marker.tweet.text
-        // change the image
-        // change the color of the text for neg/pos
-        return customMarker
-    }
-    
-    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
-        print("tap tap")
-    }
+//    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+//        print("tap tap")
+//    }
 }
--- TweetPinPointer/MarkerCreator.swift
+++ TweetPinPointer/MarkerCreator.swift
@@ -16,6 +16,30 @@ class MarkerCreator {
     func createMaker(withTweet tweet: Tweet) {
         let position = CLLocationCoordinate2DMake(tweet.latitude, tweet.longitude)
         let marker = GMSMarker(position: position)
+        let customMarker: TweetInfoWindow = TweetInfoWindow.loadFromNib()
+        customMarker.text.text = tweet.text
+        // change the image
+        // change the color of the text for neg/pos
+        
+        let icon = UIImage(named: "Marker")!
+        
+        // Create container
+        let annotationImage = UIView.init(frame: CGRect(x: 0, y: 0, width: customMarker.frame.size.width, height: customMarker.frame.size.height + icon.size.height))
+        annotationImage.addSubview(customMarker)
+
+        // Create icon view and center
+        let iconImageView = UIImageView(image: icon)
+        iconImageView.backgroundColor = .clear
+        iconImageView.frame = CGRect(x: (customMarker.frame.size.width - icon.size.width)/2, y: customMarker.frame.size.height, width: icon.size.width, height: icon.size.height)
+        annotationImage.addSubview(iconImageView)
+        
+        // Render image
+        UIGraphicsBeginImageContextWithOptions(annotationImage.frame.size, false, UIScreen.main.scale)
+        if let ctx = UIGraphicsGetCurrentContext() {
+            annotationImage.layer.render(in: ctx)
+            marker.icon = UIGraphicsGetImageFromCurrentImageContext()
+        }
+    
         marker.tweet = tweet
         marker.map = mapView
     }
