3cc8b7f444991d5790f5afd75d57c3b360e76028
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-10-01 19:35:41 -0500

    Got current location

--- TweetPinPointer/MapsController.swift
+++ TweetPinPointer/MapsController.swift
@@ -9,12 +9,50 @@
 import UIKit
 import GoogleMaps
 
-class MapsController: UIViewController {
+class MapsController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager:CLLocationManager!
+    var userLati: CLLocationDegrees!
+    var userLong: CLLocationDegrees!
     
-    // You don't need to modify the default init(nibName:bundle:) method.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        determineMyCurrentLocation()
+    }
     
-    override func loadView() {
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+    func determineMyCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        manager.stopUpdatingLocation()
+        
+        userLati = userLocation.coordinate.latitude
+        userLong = userLocation.coordinate.longitude
+        loadMap()
+        
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
+    }
+    
+    func loadMap() {
+        let camera = GMSCameraPosition.camera(withLatitude: userLati, longitude: userLong, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
         view = mapView
